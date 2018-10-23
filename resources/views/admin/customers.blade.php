c7a8f7aea442e7da71dac1896bd32eb5353a7e20
techkartik <tech@itconsultingcompany.com.au>
2018-10-23 10:18:49 +0530

    supplier export

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -9,8 +9,6 @@
 <script type="text/javascript" src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
 <!-- <script type="text/javascript" src="{{ asset('assets/js/dataTables.bootstrap.js') }}"></script> -->
 
-<script type="text/javascript" src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
-
 <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
--- resources/views/admin/supplier-list.blade.php
+++ resources/views/admin/supplier-list.blade.php
@@ -5,23 +5,23 @@
 	border-radius: 0; 
 }
 </style>
-<script type="text/javascript" src="{{ asset('assets/js/2jquery.dataTables.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/3dataTables.bootstrap.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/plugins/export-sheet/tableExport.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/plugins/export-sheet/jquery.base64.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/plugins/export-sheet/jspdf/libs/sprintf.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/plugins/export-sheet/jspdf/jspdf.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/plugins/export-sheet/jspdf/libs/base64.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
+
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 
 <div class="wizard">
 
 	<div class="col-md-4 col-lg-4 col-sm-4 col-xs-12 pull-left mt-5">
-		<h3 class="text-left">Supplier List / Results (Products)</h3>
+		<h3 class="text-left">Supplier List / Results</h3>
 	</div>
 	<div class="col-md-8 col-lg-8 col-sm-8 col-xs-12 mt-15">
 		<div class="row">
 			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 pull-right">
-				<div class="dropdown export">
+				<div class="dropdown export" id="buttons">
 					<button class="btn btn-dark m-l-5 btn-block dropdown-toggle" type="button" data-toggle="dropdown">EXPORT DATA OPTIONS
 						<span class="caret"></span></button>
 						<ul class="dropdown-menu">
@@ -191,8 +191,44 @@
 		</div>
 		<script type="text/javascript">
 			$(document).ready(function() {
-				$('#customers').DataTable();
-			});
+			var table = $('#customers').DataTable();
+            var buttons = new $.fn.dataTable.Buttons(table, {
+                 extend: 'collection',
+                 text: 'Export', 
+                 buttons: [
+                 $.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'excelHtml5',
+                    className: 'btn btn-success',
+                    exportOptions: {
+                        columns: [0,1,2,3,4,5,6,7,8]
+                    }
+                } ),   
+                $.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'csvHtml5',
+                    className: 'btn btn-danger',
+                    exportOptions: {
+                        columns: [0,1,2,3,4,5,6,7,8]
+                    }
+                } ),
+                $.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'pdfHtml5',
+                    className: 'btn btn-danger',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL',
+                    exportOptions: {
+                        columns: [0,1,2,3,4,5,6,7,8,9]
+                    }
+                } )
+                ]
+            }).container().appendTo($('#buttons'));           
+        } );
+			
 
 		</script>
 		@endsection
