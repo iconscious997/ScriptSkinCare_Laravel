158a7eeb2ab93cbc7535b6144d04af4293449866
nirajlad <niraj@itconsultingcompany.com.au>
2018-10-23 12:45:00 +0530

    add supplier list2 page

--- resources/views/admin/supplier-list2.blade.php
+++ resources/views/admin/supplier-list2.blade.php
@@ -4,36 +4,47 @@
 .pagination > li > a, .pagination > li > span {
 	border-radius: 0; 
 }
+@media only screen and (min-device-width : 768px) and (max-device-width : 1024px) 
+and (orientation : portrait) { 
+	#footer {
+		position: relative;
+	}
+}
 </style>
-<script type="text/javascript" src="{{ asset('assets/js/2jquery.dataTables.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/3dataTables.bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
+
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 
 
 <div class="wizard">
-	<div class="container">
+	<div class="col-md-4 col-lg-4 col-sm-4 col-xs-12 pull-left mt-5">
+		<h3 class="text-left">Supplier List / Results (Products)</h3>
+	</div>
+	<div class="col-md-8 col-lg-8 col-sm-8 col-xs-12 mt-15">
 		<div class="row">
-			<div class="col-md-4 pull-left mt-5">
-				<h3 class="text-left">Supplier List / Results (Products)</h3>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 pull-right">
+				<div class="dropdown export">
+					<button class="btn btn-default m-l-5 btn-block dropdown-toggle" type="button" data-toggle="dropdown">EXPORT DATA OPTIONS
+						<span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu"  id="buttons"></ul>
+				</div>
 			</div>
-			<div class="col-md-8 mt-15 text-right">
-				<div class="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-					<button class="btn btn-green m-l-5 btn-block"> SORT / ARRANGE BY</button>
-				</div>
-				<div class="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-					<button class="btn btn-default m-l-5 btn-block"> EXPORT DATA OPTIONS</button>
-				</div>
-				<div class="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-					 <a href="{{ url('/user-supplier-add') }}"><button class="btn btn-green m-l-5 btn-block"> + ADD NEW SUPPLIER</button></a>
-				</div>
-				<div class="col-md-3 col-lg-3 col-sm-3 col-xs-6">
-					<button class="btn btn-default m-l-5 btn-block"> + ADD NEW PRODUCT</button>
-				</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 pull-right">
+				<a href="{{ url('/user-supplier-add') }}"><button class="btn btn-default m-l-5 btn-block"> + ADD NEW SUPPLIER</button></a>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 pull-right">
+				<button class="btn btn-default m-l-5 btn-block"> + ADD NEW PRODUCT</button>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="content-fix ">
-	<div class="table-responsive mb-30">
+	<div class="table-responsive mb-40">
 		<table class="table display" id="customers">
 			<thead class="thead-dark">
 				<tr>
@@ -239,8 +250,45 @@
 	</div>
 	<script type="text/javascript">
 		$(document).ready(function() {
-			$('#customers').DataTable();
+			var table = $('#customers').DataTable();
+			var buttons = new $.fn.dataTable.Buttons(table, {
+				extend: 'collection',
+				text: 'Export', 
+				buttons: [
+				$.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'excelHtml5',
+                    // className: 'btn btn-success',
+                    exportOptions: {
+                    	columns: [0,1,2,3,4,5,6,7,8]
+                    }
+                } ),   
+				$.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'csvHtml5',
+                    // className: 'btn btn-danger',
+                    exportOptions: {
+                    	columns: [0,1,2,3,4,5,6,7,8]
+                    }
+                } ),
+				$.extend( true, {}, {
+                    // footer: true,
+                    title: 'Suppliers',
+                    extend: 'pdfHtml5',
+                    // className: 'btn btn-danger',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL',
+                    exportOptions: {
+                    	columns: [0,1,2,3,4,5,6,7,8,9]
+                    }
+                } )
+				]
+			}).container().appendTo($('#buttons'));           
 		} );
+		
+
 	</script>
 	@endsection
 
