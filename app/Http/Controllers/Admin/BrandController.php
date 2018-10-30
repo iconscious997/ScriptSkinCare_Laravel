f539c4d58cad7ed6afd53fdf63f39a25037f1ade
Ravi Dige <ravi@itconsultingcompany.com.au>
2018-10-30 16:42:30 +0530

    add brand

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -30,207 +30,286 @@ class BrandController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->isMethod('get')) {            
+
+        if ($request->isMethod('get')) {
+            
 
             $data = Brand::join('company_details','brands.brand_company_id','=','company_details.id')->select('brands.brand_name','brands.id','brands.brand_logo','company_details.business_name')->get();
+
+
         }
 
         if ($request->isMethod('post')) {
 
-            if (isset($request->business_name) && !empty($request->business_name)) {            
-              $query[]=['company_details.business_name', 'like','%' . $request->business_name. '%'];
-            }
-
-            if (isset($request->brand_name) && !empty($request->brand_name)) {            
-               $query[]=['brands.brand_name', 'like','%'. $request->brand_name.'%'];
-            }
-
-            if (isset($request->status) && !empty($request->status)) {        
-                $query[]=['brands.status', '=',$request->status];
-            }
-
-            if(isset($query) && !empty($query)){
-
-                $d = Brand::join('company_details','brands.brand_company_id','=','company_details.id')
-                ->where($query);
-                
-                if (isset($request->create_date) && !empty($request->create_date)) {        
-                    $d->whereDate('created_date', '=', date("Y-m-d", strtotime($request->create_date)) );       
-                }
-
-                $data = $d->get();
-
-            }else if(isset($request->create_date) && !empty($request->create_date)){
-
-                $data = Brand::join('company_details','brands.brand_company_id','=','company_details.id')
-                ->whereDate('brands.created_date', '=', date("Y-m-d", strtotime($request->create_date)) )
-                ->select('brands.brand_name','brands.id','brands.brand_logo','company_details.business_name')->get(); 
-
-            }else{
-
-                $data = Brand::join('company_details','brands.brand_company_id','=','company_details.id')->select('brands.brand_name','brands.id','brands.brand_logo','company_details.business_name')->get();
-            }
+           if (isset($request->business_name) && !empty($request->business_name)) {
+            
+            
+            $query[]=['company_details.business_name', 'like','%' . $request->business_name. '%'];
+            
         }
 
-        return view('admin.brand-list',compact('data','request'));
+        if (isset($request->brand_name) && !empty($request->brand_name)) {
+            
+           $query[]=['brands.brand_name', 'like','%'. $request->brand_name.'%'];
+       }
+
+
+       if (isset($request->status) && !empty($request->status)) {
+        
+        $query[]=['brands.status', '=',$request->status];
     }
-   
 
-    public function brandedit($id)
-    {        
-        $brands_data = Brand::where('id', $id)->first(); 
-        $sub_supplier=Supplier::where('company_id', $brands_data->brand_company_id)->get();
+    if(isset($query) && !empty($query)){
 
-        $supplier = Supplier::join('role_user','supplier_details.user_id','=','role_user.user_id')
-        ->join('roles','role_user.role_id','=','roles.id')
-        ->where('supplier_details.company_id', $brands_data->brand_company_id)
-        ->where('roles.name', "supplier_admin")
-        ->select('supplier_details.id','supplier_details.first_name','supplier_details.last_name')
-        ->get()->toArray();
+        
 
-        $result = Supplier::where('company_id', $brands_data->brand_company_id)->select('id','brand_ids')->get()->toArray();
+       $d = Brand::join('company_details','brands.brand_company_id','=','company_details.id')
+       ->where($query);
+       if (isset($request->create_date) && !empty($request->create_date)) {
+        
+        $d->whereDate('created_date', '=', date("Y-m-d", strtotime($request->create_date)) );
+        
+    }
+
+    $data = $d->get();
+
+}else if(isset($request->create_date) && !empty($request->create_date)){
+
+
+  $data = Brand::join('company_details','brands.brand_company_id','=','company_details.id')
+  ->whereDate('brands.created_date', '=', date("Y-m-d", strtotime($request->create_date)) )
+  ->select('brands.brand_name','brands.id','brands.brand_logo','company_details.business_name')->get();
+  
+
+}else{
+
+  $data = Brand::join('company_details','brands.brand_company_id','=','company_details.id')->select('brands.brand_name','brands.id','brands.brand_logo','company_details.business_name')->get();
+}
+
+
+
+}
+
+
+
+return view('admin.brand-list',compact('data','request'));
+}
+
+    // public function brandadd()
+    // {
+    //     return view('admin.brandadd');
+    // }
+
+public function brandedit($id)
+{        
+    $brands_data = Brand::where('id', $id)->first(); 
+    $sub_supplier=Supplier::where('company_id', $brands_data->brand_company_id)->get();
+
+    $supplier = Supplier::join('role_user','supplier_details.user_id','=','role_user.user_id')
+    ->join('roles','role_user.role_id','=','roles.id')
+    ->where('supplier_details.company_id', $brands_data->brand_company_id)
+    ->where('roles.name', "supplier_admin")
+    ->select('supplier_details.id','supplier_details.first_name','supplier_details.last_name')->get()->toArray();
+
+    $result = Supplier::where('company_id', $brands_data->brand_company_id)->select('id','brand_ids')->get()->toArray();
     
-        foreach ($result as  $value) {        
-            if ($value['brand_ids']!=null) {
-                $tmp = explode(',', $value['brand_ids']);
+    foreach ($result as  $value) {
+        
+        if ($value['brand_ids']!=null) {
+            
 
-                if (in_array($id, $tmp)) {                
-                    $update_supplier[]= $value['id'];
-                }
-            }      
+            $tmp = explode(',', $value['brand_ids']);
+
+            if (in_array($id, $tmp)) {
+                
+               $update_supplier[]= $value['id'];
+           }
+
+
+       }
        
-        }
-           
-        return view('admin.brandedit',compact('brands_data','supplier','sub_supplier','update_supplier'));
-    }
+       
+   }
+        // echo $brands_data->brand_company_id;
+        // dd($supplier_data);
+   
+   return view('admin.brandedit',compact('brands_data','supplier','sub_supplier','update_supplier'));
+}
 
-    
-    public function brandstore(Request $request)
-    {   
+    // public function brandactivedeactive($id,$status){
+
+    //     DB::table('client_details')
+    //     ->where("client_details.id", '=',  $id)
+    //     ->update(['client_details.status'=> $status]);
+
+    //      return redirect('/brands');
+    // }
+
+
+public function brandstore(Request $request)
+{   
+
  
-        if( !empty( $request->id ) ) { 
+    if( !empty( $request->id ) ) { 
 
-            $validatedData = $request->validate([
-                'brand_name'     => 'required',
-                'supplier_parent_id' => 'required',
-                'created_by'     => 'required',
-                'assign_to_user' => 'required',
-                'brand_logo'     => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-                'approved_by'    =>  'required',
-
-            ]);
-
-            if ($request->file('brand_logo')) {
-                $randomNumber = rand(1, 10000);
-
-                $imageName = 'brand'.$randomNumber.'.'.$request->file('brand_logo')->getClientOriginalExtension();
-
-                $request->file('brand_logo')->move(
-                    base_path() . '/public/images/brand', $imageName
-                );
-
-            } else {
-                $imageName = $request->old_image_name;
-            }
-
-            $brands = Brand::find($request->id);
-            $brands->brand_name = $request->brand_name;
-            $brands->supplier_parent_id = $request->supplier_parent_id;
-            $brands->brand_logo = $imageName;
-            $brands->user_added_by= $request->created_by;
-            $brands->approved_by= $request->approved_by;
-            $brands->modified_by= \Auth::user()->id;
-            $brands->save();
-
-            $sub_supplier = Supplier::where('company_id', $brands->brand_company_id)->get();
-        
-            foreach ($sub_supplier as $key => $value) {
-                $add_brand_tmp=array();
-                $tmp_remove = explode(',', $value->brand_ids);
-
-                foreach ($tmp_remove as $sub) {                
-                    if( $sub!=$brands->id) {
-                        array_push($add_brand_tmp, $sub);                    
-                    }
-                }
-
-                $add_brand = implode(',', $add_brand_tmp);              
-
-                $supplier = Supplier::find($value->id);
-                $supplier->brand_ids       = $add_brand;
-                $supplier->save();
-            }        
-
-            foreach ($request->assign_to_user as $key => $value) {        
-
-                $supplier = Supplier::find($value);
-                $tmp = explode(',', $supplier->brand_ids);
-                
-                if( !in_array($brands->id, $tmp) ) {
-                    array_push($tmp, $brands->id);
-                    $supplier->brand_ids       = implode(',', $tmp);
-                }else if (in_array($brands->id, $tmp)) {
-                    $supplier->brand_ids  =implode(',', $tmp);
-                }else{            
-                    $supplier->brand_ids       =$brands->id;
-                }
-        
-                $supplier->save();
-            }      
-
-            setflashmsg('Brand Updated Successfully','1');
-            return redirect('/supplier-brand-list');     
-        }
-    }
-
-
-    public function addnewbrand()
-    {
-        $company = Company::all();
-        return view('admin.add-new-brand',compact('company'));
-    }
-
-
-    public function addnewbrandstore(Request $request)
-    {
-                // check for validation
         $validatedData = $request->validate([
             'brand_name'     => 'required',
-            'company_id'     => 'required',
+            'supplier_parent_id' => 'required',
+            'created_by'     => 'required',
+            'assign_to_user' => 'required',
             'brand_logo'     => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'approved_by'    =>  'required',
+
         ]);
 
         if ($request->file('brand_logo')) {
-            $randomNumber = time()."_".rand(1000, 9999);
+            $randomNumber = rand(1, 10000);
+
             $imageName = 'brand'.$randomNumber.'.'.$request->file('brand_logo')->getClientOriginalExtension();
+
             $request->file('brand_logo')->move(
+                base_path() . '/public/images/brand', $imageName
+            );
+        } else {
+            $imageName = $request->old_image_name;
+        }
+        $brands = Brand::find($request->id);
+        $brands->brand_name = $request->brand_name;
+        $brands->supplier_parent_id = $request->supplier_parent_id;
+        $brands->brand_logo = $imageName;
+        $brands->user_added_by= $request->created_by;
+        $brands->approved_by= $request->approved_by;
+        $brands->modified_by= \Auth::user()->id;
+        $brands->save();
+
+        $sub_supplier = Supplier::where('company_id', $brands->brand_company_id)->get();
+        
+        foreach ($sub_supplier as $key => $value) {
+            $add_brand_tmp=array();
+            $tmp_remove = explode(',', $value->brand_ids);
+
+            foreach ($tmp_remove as $sub) {
+                
+                if( $sub!=$brands->id) {
+                    array_push($add_brand_tmp, $sub);
+
+                    
+                }
+
+                        // echo $sub;
+            }
+
+            $add_brand      = implode(',', $add_brand_tmp);
+
+                // $pos = array_search($brands->id, $tmp_remove);
+                // $result = remove_element($tmp_remove,"'".$brands->id."'");
+                // dump($result);
+                // $pos;
+
+            $supplier = Supplier::find($value->id);
+            $supplier->brand_ids       = $add_brand;
+            $supplier->save();
+        }
+
+        
+
+        foreach ($request->assign_to_user as $key => $value) {
+         
+
+            $supplier = Supplier::find($value);
+            $tmp = explode(',', $supplier->brand_ids);
+                // dump($tmp);
+            if( !in_array($brands->id, $tmp) ) {
+                array_push($tmp, $brands->id);
+
+                $supplier->brand_ids       = implode(',', $tmp);
+            }else if (in_array($brands->id, $tmp))
+            {
+               $supplier->brand_ids  =implode(',', $tmp);
+           }
+           else{
+            
+            $supplier->brand_ids       =$brands->id;
+        }
+        
+        $supplier->save();
+    }
+
+    
+
+    
+
+    setflashmsg('Record Updated Successfully','1');
+    return redirect('/supplier-brand-list'); 
+
+    
+}
+
+
+
+}
+
+
+public function addnewbrand()
+{
+    $company = Company::all();
+    return view('admin.add-new-brand',compact('company'));
+}
+
+
+public function addnewbrandstore(Request $request)
+{
+    
+
+
+            // check for validation
+    $validatedData = $request->validate([
+        'brand_name'     => 'required',
+        'company_id'     => 'required',
+        'brand_logo'     => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+    ]);
+
+    if ($request->file('brand_logo')) {
+        $randomNumber = time()."_".rand(1000, 9999);
+
+        $imageName = 'brand'.$randomNumber.'.'.$request->file('brand_logo')->getClientOriginalExtension();
+
+        $request->file('brand_logo')->move(
             base_path() . '/public/images/brand', $imageName
         );
 
-        } else {
-            $imageName='img_avatar1.png';
-        }
-    
-        // now add data to supplier_details table
-        $brands = Brand::create([
-            'brand_name'               => $request->brand_name,
-            'brand_company_id'         => $request->company_id,
-            'brand_logo'               => $imageName,
-            'user_added_by'            => 0,
-            'approved_by'              => 0,
-            'status'                   => 0,
-            'created_date'             => date('Y-m-d H:i:s'),
-            'created_by'               => \Auth::user()->id,
-            'modified_by'              => \Auth::user()->id,
-        ]);
-
-        if( !empty($brands->exists) ) {        
-            setflashmsg('Brand Added Successfully','1');
-            return redirect('/supplier-brand-list'); 
-        } else {
-            setflashmsg('Some error occured. Please try again','0');
-            return redirect('/add-new-brand');        
-        }
+    } else {
+        $imageName='img_avatar1.png';
     }
+    
+            // now add data to supplier_details table
+    $brands = Brand::create([
+        'brand_name'               => $request->brand_name,
+        'brand_company_id'         => $request->company_id,
+        'brand_logo'               => $imageName,
+        'user_added_by'            => 0,
+        'approved_by'              => 0,
+        'status'                   => 0,
+        'created_date'             => date('Y-m-d H:i:s'),
+        'created_by'               => \Auth::user()->id,
+        'modified_by'              => \Auth::user()->id,
+    ]);
+
+    if( !empty($brands->exists) ) {
+                // assign 
+     
+                // success
+        
+        setflashmsg('Record Inserted Successfully','1');
+        return redirect('/supplier-brand-list'); 
+    } else {
+        setflashmsg('Some error occured. Please try again','0');
+        return redirect('/add-new-brand');
+        
+    }
+
+
+}
+
 
 }
