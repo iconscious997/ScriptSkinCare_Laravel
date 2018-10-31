1fc8fe552054cdeba8865ff647323d75b6dfcdce
Ravi Dige <ravi@itconsultingcompany.com.au>
2018-10-31 15:01:15 +0530

    validations issue

--- resources/views/admin/add-new-user.blade.php
+++ resources/views/admin/add-new-user.blade.php
@@ -16,12 +16,12 @@
                 <div class="col-md-10">
                     <div class="col-md-6">
                         <div class="row">
-                            
+
                         </div>
                     </div>
                     <div class="col-md-6 mt-10">
                         <div class="row">
-                           
+
                         </div>
                     </div>
                 </div>
@@ -35,9 +35,9 @@
                             @if ( !empty($supplier) )
                             <input type="hidden" name="id" value="{{ $supplier->id }}">
                             @endif
-                         
 
-                           
+
+
                             {{-- Session::put('supplier_parent_id', Session::get('second')); --}}
                             <input type="hidden" name="whattodo" id="whattodo" value="update">
                             <input type="hidden" name="savestep" id="savestep" value="0">
@@ -74,96 +74,102 @@
 
                             <select name="user_parent_id" id="user_parent_id">
                                 <option value="">Select Supplier User</option>
-                               
+
+                                @foreach( $old_supplier as $role )
+                                <option  value="{{ $role['id'] }}" {{old('user_parent_id')==$role['id']?'selected':''}} >{{ $role['first_name'] }} {{ $role['last_name'] }}</option>
+
+                                @endforeach
+
+
                             </select>
                             @if ($errors->has('user_parent_id'))
                             <span class="inputError">{{ $errors->first('user_parent_id') }}</span>
                             @endif
                         </div>
-                       
-                    </div>
-                        <div class="row">
-
-                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="business_tel_number" id="business_tel_number" placeholder="Business Telephone No:" value="{{ !empty($supplier->business_tel_number) ? $supplier->business_tel_number : old('business_tel_number') }}">
-                            @if ($errors->has('business_tel_number'))
-                            <span class="inputError">{{ $errors->first('business_tel_number') }}</span>
-                            @endif
-                        </div>
-                        
-                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="position" id="position" placeholder="Position :" value="{{ !empty($supplier->position) ? $supplier->position : old('position') }}">
-                            @if ($errors->has('position'))
-                            <span class="inputError">{{ $errors->first('position') }}</span>
-                            @endif
-                        </div>
 
                     </div>
+                    <div class="row">
 
-                    <div class="row">
-                        <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="business_address_line_1" id="business_address_line_1" placeholder="Business Address:" value="{{ !empty($supplier->business_address_line_1) ? $supplier->business_address_line_1 : old('business_address_line_1') }}">
-                            @if ($errors->has('business_address_line_1'))
-                            <span class="inputError">{{ $errors->first('business_address_line_1') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="business_address_line_2" id="business_address_line_2" placeholder="Business Address(field 2):" value="{{ !empty($supplier->business_address_line_2) ? $supplier->business_address_line_2 : old('business_address_line_2') }}">
-                            @if ($errors->has('business_address_line_2'))
-                            <span class="inputError">{{ $errors->first('business_address_line_2') }}</span>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-md-6">
-                           <select name="user_role" id="user_role">
-                            <option value="">Select User Role</option>
-                            @foreach( $roles as $role )
-                            <option  value="{{ $role->id }}" {{old('user_role')==$role->id?'selected':''}} >{{ $role->label }}</option>
-                            @endforeach
-                          </select>
-                          @if ($errors->has('user_role'))
-                          <span class="inputError">{{ $errors->first('user_role') }}</span>
-                          @endif
-                        </div>
-                        <div class="form-group col-md-6">
-                            <input type="text" class="form-control" name="mobile_number" id="mobile_number" placeholder="Mobile No:" value="{{ !empty($supplier->mobile_number) ? $supplier->mobile_number : old('mobile_number') }}">
-                            @if ($errors->has('mobile_number'))
-                            <span class="inputError">{{ $errors->first('mobile_number') }}</span>
-                            @endif
-                        </div>
-                         
-                    </div>
-                    <div class="row">
                        <div class="form-group col-md-6">
-                            <input type="text" class="form-control" {{ !empty($user->email) ? 'readonly' : '' }} name="email" id="email" placeholder="Email Address:" autocomplete="new-password" value="{{ !empty($user->email) ? $user->email : old('email') }}">
-                            @if ($errors->has('email'))
-                            <span class="inputError">{{ $errors->first('email') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group col-md-6">
-                            <input type="password" {{ !empty($user->email) ? 'readonly' : '' }} class="form-control" name="password" id="password" placeholder="Password:" autocomplete="new-password" value="{{ !empty($user->email) ? '******' : '' }}">
-                            @if ($errors->has('password'))
-                            <span class="inputError">{{ $errors->first('password') }}</span>
-                            @endif
-                        </div>
+                        <input type="text" class="form-control" name="business_tel_number" id="business_tel_number" placeholder="Business Telephone No:" value="{{ !empty($supplier->business_tel_number) ? $supplier->business_tel_number : old('business_tel_number') }}">
+                        @if ($errors->has('business_tel_number'))
+                        <span class="inputError">{{ $errors->first('business_tel_number') }}</span>
+                        @endif
                     </div>
 
-                    <div class="col-md-1">&nbsp;</div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" name="position" id="position" placeholder="Position :" value="{{ !empty($supplier->position) ? $supplier->position : old('position') }}">
+                        @if ($errors->has('position'))
+                        <span class="inputError">{{ $errors->first('position') }}</span>
+                        @endif
+                    </div>
 
                 </div>
+
+                <div class="row">
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" name="business_address_line_1" id="business_address_line_1" placeholder="Business Address:" value="{{ !empty($supplier->business_address_line_1) ? $supplier->business_address_line_1 : old('business_address_line_1') }}">
+                        @if ($errors->has('business_address_line_1'))
+                        <span class="inputError">{{ $errors->first('business_address_line_1') }}</span>
+                        @endif
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" name="business_address_line_2" id="business_address_line_2" placeholder="Business Address(field 2):" value="{{ !empty($supplier->business_address_line_2) ? $supplier->business_address_line_2 : old('business_address_line_2') }}">
+                        @if ($errors->has('business_address_line_2'))
+                        <span class="inputError">{{ $errors->first('business_address_line_2') }}</span>
+                        @endif
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-6">
+                     <select name="user_role" id="user_role">
+                        <option value="">Select User Role</option>
+                        @foreach( $roles as $role )
+                        <option  value="{{ $role->id }}" {{old('user_role')==$role->id?'selected':''}} >{{ $role->label }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('user_role'))
+                    <span class="inputError">{{ $errors->first('user_role') }}</span>
+                    @endif
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control" name="mobile_number" id="mobile_number" placeholder="Mobile No:" value="{{ !empty($supplier->mobile_number) ? $supplier->mobile_number : old('mobile_number') }}">
+                    @if ($errors->has('mobile_number'))
+                    <span class="inputError">{{ $errors->first('mobile_number') }}</span>
+                    @endif
+                </div>
+
             </div>
-          
             <div class="row">
-                <div class="col-md-1">&nbsp;</div>
-
-                <div class="col-md-10 text-center mt-30 mb-10">
-                    <button class="btn btn-dark btn-pad selected" type="button" id="btnsavestep"> SAVE CHANGES</button>
-                </div>
-                <div class="col-md-1">&nbsp;</div>
+             <div class="form-group col-md-6">
+                <input type="text" class="form-control" {{ !empty($user->email) ? 'readonly' : '' }} name="email" id="email" placeholder="Email Address:" autocomplete="new-password" value="{{ !empty($user->email) ? $user->email : old('email') }}">
+                @if ($errors->has('email'))
+                <span class="inputError">{{ $errors->first('email') }}</span>
+                @endif
+            </div>
+            <div class="form-group col-md-6">
+                <input type="password" {{ !empty($user->email) ? 'readonly' : '' }} class="form-control" name="password" id="password" placeholder="Password:" autocomplete="new-password" value="{{ !empty($user->email) ? '******' : '' }}">
+                @if ($errors->has('password'))
+                <span class="inputError">{{ $errors->first('password') }}</span>
+                @endif
             </div>
         </div>
+
+        <div class="col-md-1">&nbsp;</div>
+
     </div>
+</div>
+
+<div class="row">
+    <div class="col-md-1">&nbsp;</div>
+
+    <div class="col-md-10 text-center mt-30 mb-10">
+        <button class="btn btn-dark btn-pad selected" type="button" id="btnsavestep"> SAVE CHANGES</button>
+    </div>
+    <div class="col-md-1">&nbsp;</div>
+</div>
+</div>
+</div>
 </form>
 <div class="footer">
     <div class="conatiner text-center">
@@ -171,27 +177,27 @@
             <div class="col-md-1 col-sm-1">&nbsp;</div>
             <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                 <a href="{{ url('/supplier-company-add') }}"><button class="btn btn-default spbtn" type="button"> + ADD NEW SUPPLIER</button></a>
-                   
 
-                   
+
+
                 <a href="{{ url('/add-new-user') }}"><button class="btn btn-default spbtn m-l-20 active" type="button"> + ADD A NEW USER</button></a>
                 <a href="{{ url('/add-new-brand') }}">
-                <button class="btn btn-default spbtn m-l-20" type="button"> + ADD A NEW BRAND</button></a>
+                    <button class="btn btn-default spbtn m-l-20" type="button"> + ADD A NEW BRAND</button></a>
+                </div>
+                <div class="col-md-1 col-sm-1">&nbsp;</div>
             </div>
-            <div class="col-md-1 col-sm-1">&nbsp;</div>
         </div>
     </div>
-</div>
 
-<script>
-    $(document).ready(function() {
-        $('#btnsavestep').on('click', function(e) {
+    <script>
+        $(document).ready(function() {
+            $('#btnsavestep').on('click', function(e) {
         // set the savestep to 1 so we can track that we have to redirect to next or not
         $('#savestep').val(1);
         $('#first').submit();
     });
 
-        $('#addadditionaluser').on('click', function(e) {
+            $('#addadditionaluser').on('click', function(e) {
         // clear all fields
         $('#first_name').val('');
         $('#last_name').val('');
@@ -206,23 +212,23 @@
         $('#whattodo').val('new');
     });
 
-        
-    });
 
-    function supplier_user(id) {
+        });
+
+        function supplier_user(id) {
         // body...
         if (id) {
 
             $.ajax({
 
-            type:"GET",
-            url:"<?php echo url('/get_supplier_company')?>/"+id,
-             success: function(data) {  
-                   
-                    
+                type:"GET",
+                url:"<?php echo url('/get_supplier_company')?>/"+id,
+                success: function(data) {  
+
+
                     $('#user_parent_id').html(data);
                 },
-        });
+            });
             
         }else{
 
